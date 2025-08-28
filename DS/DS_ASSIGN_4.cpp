0ce9873f56403c98bbc5ea86a0d32d0ea4e7fcd9
tusharborate46 <boratetushar46@gmail.com>
2025-08-28 16:19:23 +0530

    DS_ASSIGN_4.cpp

--- DS/DS_ASSIGN_4.cpp
+++ DS/DS_ASSIGN_4.cpp
@@ -3,32 +3,51 @@ using namespace std;
 
 class node{
 	public:
-	int data;
+	int priority;
+	int est_time;
 	node*next;
 };
+void newnode(node* &head,int priority,int est_time){
+	node* newnode=new node;
+	{
+	newnode->priority=priority;
+	newnode->est_time=est_time;
+	newnode->next=NULL;
+}
+if(!head || head->priority<priority){
+	newnode->next=head;
+	head=newnode;
+	return;
+}
+node* temp=head;
+
+while(temp->next&&temp->next->priority>=priority){
+	temp=temp->next;
+}
+newnode->next=temp->next;
+temp->next=newnode;
+}
+void ext_node(node* &head){
+	while(head){
+		cout<<"Tasks with priority "<<head->priority<<" in time "<<head->est_time<<endl;
+		node* temp1=head;
+		head=head->next;
+		delete temp1;
+	}
+}
 
 int main(){
-	//node* head=NULL;
-	node* head=new node;
-	head->data=0;
-	node* first=new node;
-	first->data=5;
+	node* task_scheduler=NULL;
+	newnode(task_scheduler,4,200);
+	newnode(task_scheduler,1,150);
+	newnode(task_scheduler,5,180);	
+	newnode(task_scheduler,1,250);
+	newnode(task_scheduler,2,120);
+	newnode(task_scheduler,6,350);
+	newnode(task_scheduler,3,100);
 	
-	node* second=new node;
-	second->data=10;
-	
-	head->next=first;
-	first->next=second;
-	
-	second->next=NULL;
-	
-	cout<<head->next<<endl;
-	
-	cout<<first->data<<endl;
-	cout<<first->next<<endl;
-	
-	cout<<second->data<<endl;
-	cout<<second->next;
+	ext_node(task_scheduler);
+
 	
 	
 	return 0;
